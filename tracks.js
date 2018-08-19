fdea3e14bc9fbbfe3ca6ce2a84654b0b2c4b947c
dhrandy <dhrandy@gmail.com>
2018-08-19 15:27:16 -0400

    added date

--- tracks.js
+++ tracks.js
@@ -3,17 +3,20 @@
     {
         "id": 1,
         "track":"Track 1",
-        "location": "Charlotte, NC"
+        "location": "Charlotte, NC",
+        "date": "11/23/2018"
 },
 {
         "id":2,
         "track":"Track 2",
-        "location": "Concord, NC"
+        "location": "Concord, NC",
+        "date": "11/22/2018"
 },
 {
         "id":3,
         "track":"Track 3",
-        "location":"Kannaplis, NC"
+        "location":"Kannaplis, NC",
+        "date": "11/21/2018"
 }
 ]
 }
