b86e7771afe45c126bb64efbf591f6212c9545ae
Randy Gentry <dhrandy@gmail.com>
2018-08-20 07:09:21 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $.getJSON(
     "https://www.randy-gentry.com/jsontest/assets/json/tracks.json",
     function(data) {
+        
+      //Only change the following line to how many races you have in the json file.
+      var races = 4;
 
       // console.log(data)
 
@@ -8,7 +11,7 @@ $.getJSON(
 
       var i = 0;
 
-      for (i=0; i<4; i++) {
+      for (i=0; i<races; i++) {
         var track = $('<td>').text(data.tracks[i].track);
         var location = $('<td>').text(data.tracks[i].location);
         var date = $('<td>').text(data.tracks[i].date);
