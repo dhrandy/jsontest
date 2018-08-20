3ed03cceb1c6cbb8713c3eb5f46a3284df725153
dhrandy <dhrandy@gmail.com>
2018-08-20 06:38:43 -0400

    uploading rest of the files

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$.getJSON(
+    "http://www.randy-gentry.com/jsontest/assets/json/tracks.json",
+    function(data) {
+
+      // console.log(data)
+
+      var trackInfo = $("#track")
+
+      var i = 0;
+
+      for (i=0; i<5; i++) {
+        var track = $('<td>').text(data.tracks[i].track);
+        var location = $('<td>').text(data.tracks[i].location);
+        var date = $('<td>').text(data.tracks[i].date);
+        var border = $('<hr>')
+        var row = $('<tr>');
+
+        row.append(row, track, location, date);
+        trackInfo.append(row);
+
+        // console.log("track= " + data.tracks[0].track );
+
+      };
+      
+    }
+);
