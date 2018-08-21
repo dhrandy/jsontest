298d9321d86a12befb2760356b33d95b43bae7be
Randy Gentry <dhrandy@gmail.com>
2018-08-21 18:22:18 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $.getJSON(
     function(data) {
         
       //Only change the following line to how many races you have in the json file.
-      var races = 4;
+      // var races = 4;
 
       // console.log(data)
 
@@ -11,7 +11,7 @@ $.getJSON(
 
       var i = 0;
 
-      for (i=0; i<races; i++) {
+      for (i=0; i<data.tracks.length; i++) {
         var track = $('<td>').text(data.tracks[i].track);
         var location = $('<td>').text(data.tracks[i].location);
         var date = $('<td>').text(data.tracks[i].date);
